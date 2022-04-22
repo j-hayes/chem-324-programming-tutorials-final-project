a3030db107fa198e20b04f493d247d2fa6067dcb
Jackson Hayes <jacksonjhayes@protonmail.com>
2022-04-22 11:12:43 -0500

    update with runner

--- main.jl
+++ main.jl
@@ -9,5 +9,9 @@ function run_calculation(input_file)
     
 end
 run_calculation("water.json")
-random_plot = plot(rand(10,5))
+
+
+random_plot = plot(rand(10,1), title = "H2 Hartree Fock Discociation Curve", label = "Hartree Fock")
+xlabel!("Internuclear Radius (a.u.)")
+ylabel!("Energy (hartree)")
 savefig(random_plot, "randomplot")      

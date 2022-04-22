0d11e362eaba34b12e3e7c2f3d4b1527716ba60e
Jackson Hayes <jacksonjhayes@protonmail.com>
2022-04-22 13:06:35 -0500

    stuff I need

--- main.jl
+++ main.jl
@@ -1,14 +1,19 @@
 using JuliaChem
 using Plots
-include("full_repl.jl")
+include("hartree_fock_runner.jl")
 
-function run_calculation(input_file)
-    JuliaChem.initialize() 
-    energy, properties = full_rhf(input_file)      
-    JuliaChem.finalize()   
+function run_calculation(distance)
+    start_calculations()
+    
+    energy = run_h2_hartree_fock(distance)     
+    println(energy)
+    energy2 = run_h2_hartree_fock(distance*2)      
+    println(energy2)
+    
+    start_calculations()  
     
 end
-run_calculation("water.json")
+run_calculation(.9)
 
 
 random_plot = plot(rand(10,1), title = "H2 Hartree Fock Discociation Curve", label = "Hartree Fock")
